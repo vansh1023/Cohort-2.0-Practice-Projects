95073c49c6f07b16f946e949f498994daf65bfe9
Vansh Choudhary <vanshchaudhary1023@gmail.com>
2026-02-15 09:53:38 +0530

    level 1 completed

---
+++ JSPractice/script.js
@@ -0,0 +1,89 @@
+// level 1
+
+// for (let i=0 ; i<10; i++){
+//     console.log(i);
+// }
+
+
+
+
+
+
+// for (let i=2 ; i<21; i++){
+//     if(i % 2 === 0){
+//         console.log(i);
+//     }
+// }
+
+
+
+
+
+
+// for (let i=10 ; i>0; i--){
+//     console.log(i);
+// }
+
+
+
+
+
+
+// let num = Number(prompt("Enter number"));
+
+// if(num > 0) console.log("Positive");
+// else console.log("Negative");
+
+
+
+
+
+
+
+
+// for(let i=1; i<11; i++){
+//     console.log(`5 x ${i} = ${5*i}`);
+// }
+
+
+
+
+
+
+
+
+// let count = 0;
+// for(let i=1; i<16; i++){
+//     if(i>8){
+//         count++;
+//     }
+// }
+
+// console.log(count);
+
+
+
+
+
+
+// const pass = "vansh353";
+
+// const userpass = prompt("Enter pass");
+
+// if(userpass === null){
+//     console.log("You cancelled it");
+// }
+// else {
+//     let userpass2 = userpass.trim();
+//     if(userpass2 === ""){
+//         console.log("Empty");
+//     }
+//     else{
+//         if(userpass2 === pass){
+//             console.log("Password matched");
+//         }
+//         else {
+//             console.log("Password not matched");
+//         }
+//     }
+// }
