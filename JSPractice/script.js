cf37f7d5e446dfff6b799ce9d9921062e0156b50
Vansh Choudhary <vanshchaudhary1023@gmail.com>
2026-02-16 11:54:53 +0530

    Level 2 completed

--- JSPractice/script.js
+++ JSPractice/script.js
@@ -87,3 +87,152 @@
 //         }
 //     }
 // }
+
+
+
+
+
+// Level 2
+
+// let attempts = 0;
+// let flag = false;
+// let pass = "vanshbhai";
+
+// let password = prompt("Enter Password");
+// attempts++;
+// if(password === pass){
+//     flag = true;
+// }
+
+// while(password !== pass){
+//     if(attempts === 3){
+//         console.log("Account Locked");
+//         break;
+//     }
+//     password = prompt("Enter Password");
+//     attempts++;
+//     if(password === pass){
+//         flag = true;
+//     }
+// }
+
+// if(flag === true){
+//     console.log("Account oppened");
+// }
+
+
+
+
+
+
+
+// let count = 0;
+// let word = prompt("Enter words");
+
+// if(word === null){
+//     console.log("you cancelled it");
+// }
+
+
+// while(word !== "stop" && word !== null){
+//     if(word === "yes"){
+//         count++;
+//     }
+//     word = prompt("Enter words");
+// }
+
+// console.log(count);
+
+
+
+
+
+
+// for(let i=1; i<51; i++){
+//     if(i % 7 === 0){
+//         console.log(i);
+//     }
+// }
+
+
+
+
+
+// let sum = 0;
+// for(let i=1; i<31; i++){
+//     if(i % 2 !== 0){
+//         sum += i;
+//     }
+// }
+// console.log(sum);
+
+
+
+
+
+// let num = prompt("Enter number");
+// num = Number(num);
+// while(num % 2 !== 0){
+//     num = Number(prompt("Enter number"));
+// }
+
+
+
+
+
+// let num1 = Number(prompt("Enter first number"));
+// let num2 = Number(prompt("Enter second number"));
+// let start, end;
+
+// if(num1 < num2){
+//     start = num1;
+//     end = num2;
+// }
+// else {
+//     start = num2;
+//     end = num1;
+// }
+
+// for(let i=start; i<=end; i++){
+//     console.log(i);
+// }
+
+
+
+// let count = 0;
+
+// for(let i=1; i<21; i++){
+//     if(count === 3) break;
+//     if(i % 2 !== 0){
+//         console.log(i);
+//         count++;
+//     }
+// }
+
+
+
+
+
+
+// let balance = 1000;
+// let attempts = 0;
+
+
+// while(balance > 0 && attempts < 3){
+//     let withdrawal = Number(prompt("Enter amount to be withdrawal"));
+
+//     if(withdrawal === 0){
+//         console.error("Please enter amount");
+//         break;
+//     }
+
+//     if(balance >= withdrawal){
+//         balance -= withdrawal;
+//         attempts++;
+//         console.log(`${withdrawal} withdraw successfully`);
+//     }
+//     else {
+//         console.log("Balance is less than Withdrawal amount");
+//         break;
+//     }
+// }
