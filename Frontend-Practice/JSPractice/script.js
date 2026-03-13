e715495c4d745a816d34529b00317e4b2521d9bb
Vansh Choudhary <vanshchaudhary1023@gmail.com>
2026-03-13 21:44:24 +0530

    frontend practice Array Flatten Program completed

--- Frontend-Practice/JSPractice/script.js
+++ Frontend-Practice/JSPractice/script.js
@@ -1043,3 +1043,27 @@
 // debouncedSearch("JavaScript");
 // debouncedSearch("JavaScript Debounce");
 // debouncedSearch("JavaScript Debounce Example");
+
+
+
+
+
+// Array Flatten Program
+
+// function flattenArray(arr) {
+//   let result = [];
+
+//   arr.forEach((item) => {
+//     if (Array.isArray(item)) {
+//       result = result.concat(flattenArray(item));
+//     } else {
+//       result.push(item);
+//     }
+//   });
+
+//   return result;
+// }
+
+// const nestedArray = [1, [2, 3], [4, [5, 6]], 7];
+
+// console.log(flattenArray(nestedArray));
