668e50ef91daba0824b9bd414d75cec62eccaba6
Vansh Choudhary <vanshchaudhary1023@gmail.com>
2026-03-22 23:43:03 +0530

    implement custom filter() polyfill in JavaScript

--- Frontend-Practice/JSPractice/script.js
+++ Frontend-Practice/JSPractice/script.js
@@ -1211,3 +1211,31 @@
 // const sum = numbers.myReduce((acc, curr) => acc + curr, 0);
 
 // console.log(sum);
+
+
+
+
+
+// customFilter.js
+
+
+
+
+// Array.prototype.myFilter = function(callback) {
+//   const result = [];
+
+//   for (let i = 0; i < this.length; i++) {
+//     if (callback(this[i], i, this)) {
+//       result.push(this[i]);
+//     }
+//   }
+
+//   return result;
+// };
+
+// // Example usage
+// const numbers = [1, 2, 3, 4, 5, 6];
+
+// const evens = numbers.myFilter((num) => num % 2 === 0);
+
+// console.log(evens);
