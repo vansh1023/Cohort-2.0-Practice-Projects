40d8d280c7e601412ccfb24a19df059d0a2c438b
Vansh Choudhary <vanshchaudhary1023@gmail.com>
2026-03-27 22:24:49 +0530

    frontend JS Practice implement LRU cache using Map

--- Frontend-Practice/JSPractice/script.js
+++ Frontend-Practice/JSPractice/script.js
@@ -1479,3 +1479,59 @@
 // emitter.off("greet", greet);
 
 // emitter.emit("greet", "Vansh"); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// lruCache.js
+
+
+// class LRUCache {
+//   constructor(capacity) {
+//     this.capacity = capacity;
+//     this.cache = new Map();
+//   }
+
+//   get(key) {
+//     if (!this.cache.has(key)) return -1;
+
+//     const value = this.cache.get(key);
+//     this.cache.delete(key);
+//     this.cache.set(key, value);
+
+//     return value;
+//   }
+
+//   put(key, value) {
+//     if (this.cache.has(key)) {
+//       this.cache.delete(key);
+//     }
+
+//     this.cache.set(key, value);
+
+//     if (this.cache.size > this.capacity) {
+//       const firstKey = this.cache.keys().next().value;
+//       this.cache.delete(firstKey);
+//     }
+//   }
+// }
+
+// // Example
+// const lru = new LRUCache(2);
+
+// lru.put(1, 1);
+// lru.put(2, 2);
+// console.log(lru.get(1)); // 1
+
+// lru.put(3, 3); // removes key 2
+// console.log(lru.get(2)); // -1
